d30eaaf1559fa468af99c5a565a6081e7487dd08
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-04-11 21:47:05 +0200

    Add combined crossover and mutate method

--- src/main/kotlin/ga/Individual.kt
+++ src/main/kotlin/ga/Individual.kt
@@ -15,11 +15,14 @@ class Individual(private val genotype: List<Gene>) : Comparable<Individual> {
     private val rank = 0
     private val crowdingDistance = 0
 
+    private val List<Gene>.loci: IntRange
+        get() = this.indices
+
     fun mutate(mutationRate: Float): Individual {
         val mutatedGenotype = mutableListOf<Gene>().also { it.addAll(genotype) }
-        for (i in genotype.indices) {
+        for (locus in genotype.loci) {
             if (Random.nextFloat() < mutationRate) {
-                mutatedGenotype[i] = Gene.values().random()
+                mutatedGenotype[locus] = Gene.values().random()
             }
         }
         return Individual(mutatedGenotype)
@@ -31,8 +34,28 @@ class Individual(private val genotype: List<Gene>) : Comparable<Individual> {
         val otherOffspringGenotype = mutableListOf<Gene>().also { it.addAll(other.genotype) }
         if (Random.nextFloat() < crossoverRate) {
             val crossoverPoint = 1 + Random.nextInt(genotype.size - 1)
-            for (i in crossoverPoint until genotype.size) {
-                thisOffspringGenotype[i] = other.genotype[i].also { otherOffspringGenotype[i] = genotype[i] }
+            for (locus in crossoverPoint until genotype.size) {
+                thisOffspringGenotype[locus] = other.genotype[locus].also { otherOffspringGenotype[locus] = genotype[locus] }
+            }
+        }
+        return Pair(Individual(thisOffspringGenotype), Individual(otherOffspringGenotype))
+    }
+
+    fun crossoverAndMutate(other: Individual, crossoverRate: Float, mutationRate: Float): Pair<Individual, Individual> {
+        require(genotype.size == other.genotype.size)
+        val thisOffspringGenotype = mutableListOf<Gene>().also { it.addAll(genotype) }
+        val otherOffspringGenotype = mutableListOf<Gene>().also { it.addAll(other.genotype) }
+        if (Random.nextFloat() < crossoverRate) {
+            val crossoverPoint = 1 + Random.nextInt(genotype.size - 1)
+            for (locus in crossoverPoint until genotype.size) {
+                thisOffspringGenotype[locus] = other.genotype[locus].also { otherOffspringGenotype[locus] = genotype[locus] }
+                if (Random.nextFloat() < mutationRate) thisOffspringGenotype[locus] = Gene.values().random()
+                if (Random.nextFloat() < mutationRate) otherOffspringGenotype[locus] = Gene.values().random()
+            }
+        } else {
+            for (locus in genotype.loci) {
+                if (Random.nextFloat() < mutationRate) thisOffspringGenotype[locus] = Gene.values().random()
+                if (Random.nextFloat() < mutationRate) otherOffspringGenotype[locus] = Gene.values().random()
             }
         }
         return Pair(Individual(thisOffspringGenotype), Individual(otherOffspringGenotype))
@@ -52,11 +75,11 @@ class Individual(private val genotype: List<Gene>) : Comparable<Individual> {
     infix fun Individual.dominates(other: Individual): Boolean {
         return (
                 overallDeviation <= other.overallDeviation &&
-                edgeValue >= other.edgeValue &&
+                edgeValue <= other.edgeValue &&
                 connectivityMeasure <= other.connectivityMeasure
                 ) && (
                     overallDeviation < other.overallDeviation ||
-                    edgeValue > other.edgeValue ||
+                    edgeValue < other.edgeValue ||
                     connectivityMeasure < other.connectivityMeasure
                 )
     }
