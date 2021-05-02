dca45607b4a8aa9bbe770ffcc641cdf270244fd2
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2021-05-02 12:11:17 +0200

    Rearrange pareto front axis

--- src/main/scripts/paretoFront.py
+++ src/main/scripts/paretoFront.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.set_xlabel('overallDeviation')
-ax.set_ylabel('edgeValue')
-ax.set_zlabel('connectivityMeasure')
+ax.set_ylabel('connectivityMeasure')
+ax.set_zlabel('edgeValue')
 
 overallDeviation, edgeValue, connectivityMeasure = np.loadtxt('paretoFront.txt', delimiter=",", unpack=True)
-ax.scatter3D(overallDeviation, edgeValue, connectivityMeasure)
+ax.scatter3D(overallDeviation, connectivityMeasure, edgeValue)
 
-plt.show()
 plt.savefig("paretoFront.png")
+plt.show()
 plt.close()
